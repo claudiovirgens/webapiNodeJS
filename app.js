b31088360dcce861d9237c8aef781fbfe00249cb
Cláudio Santos das Virgens <claudiosantosvirgens@outlook.com>
2018-08-03 15:59:43 -0300

    Definindo as rotas

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const port = 3000 //porta padrão
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+//Definindo as rotas
+const router = express.Router()
+router.get('/',(requestAnimationFrame,req) => res.json({message:'Funcionando!'}))
+app.use('/',router)
