e4506b3aa4bbd2736172cabf92a1ecf7a53314c3
Cláudio Santos das Virgens <claudiosantosvirgens@outlook.com>
2018-08-03 16:33:04 -0300

    Correção de bugs

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const port = 3000 //porta padrão
+
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 //Definindo as rotas
 const router = express.Router()
-router.get('/',(requestAnimationFrame,req) => res.json({message:'Funcionando!'}))
+router.get('/',(req,res) => res.json({message:'Funcionando!'}))
 app.use('/',router)
 // Codigo para iniciar o servidor
 app.listen(port)
--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const mongoclient = require("mongodb").MongoClient
-mongoClient.connect('mongodb://localhost/workshop').then(conn => conn.db(workshop)).catch(err => console.log(err))
+const mongoClient = require("mongodb").MongoClient
+mongoClient.connect('mongodb://localhost/workshop').then(conn => global.conn = conn.db("workshop")).catch(err => console.log(err))
 
 module.export ={}
